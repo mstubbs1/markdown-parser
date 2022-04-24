38a3298d378066d70b030d466c6e77d0fe97410c
astoriama <yim016@ucsd.edu>
2022-04-24 00:49:12 -0700

    Corrected Version
    For all test cases

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -12,46 +12,31 @@ public class MarkdownParse {
         // find the next [, then find the ], then find the (, then read link upto next )
         int currentIndex = 0;
         while(currentIndex < markdown.length()) {
-            int openBracket = markdown.indexOf("[", currentIndex);
-            int closeBracket = markdown.indexOf("]", openBracket);
-            if(markdown.indexOf("(",0)!=-1){                
-                int openParen = markdown.indexOf("(", closeBracket);
-                int closeParen = markdown.indexOf(")", openParen);
-
-                int nextOpenBracket1 = markdown.indexOf("[", closeParen);
-                if(nextOpenBracket1!=-1){
-                    for(int i = closeParen; i<nextOpenBracket1;i++){
-                        if(markdown.indexOf(")", closeParen)==-1){
-                            continue;
-                        }else{
-                            if(markdown.indexOf(")", closeParen + 1) != -1 &&markdown.indexOf(")", closeParen + 1)<nextOpenBracket1){
-                                closeParen = markdown.indexOf(")", closeParen + 1);
-                            }
-                        }
-                    }
-                
+            //need All "[", "]",and"(",")", if cannot find correct, stop.
+            //1. find "[". if no, stop. Else, continue.
+            if(markdown.indexOf("[", currentIndex)==-1){
+                break;
+            }else{
+                int openBracket = markdown.indexOf("[", currentIndex);
+                //2. find "]", if no, stop
+                if(markdown.indexOf("]", openBracket)==-1){
+                    break;
                 }else{
-                    for(int i = closeParen; i<markdown.length();i++){
-                        if(markdown.indexOf(")", closeParen)==-1){
-                            continue;
+                    int closeBracket = markdown.indexOf("]", openBracket);
+                    if(markdown.indexOf("(", closeBracket)==-1){
+                        break;
+                    }else{
+                        int openParen = markdown.indexOf("(", closeBracket);
+                        if(markdown.indexOf(")", openParen)==-1){
+                            break;
                         }else{
-                         if (markdown.indexOf(")", closeParen + 1) != -1){
-                                closeParen = markdown.indexOf(")", closeParen + 1);
-                            }
+                            int closeParen = markdown.indexOf(")", openParen);
+                            toReturn.add(markdown.substring(openParen + 1, closeParen));
+                            currentIndex = closeParen + 1;
                         }
                     }
                 }
-            
-    
-                toReturn.add(markdown.substring(openParen + 1, closeParen));
-                currentIndex = closeParen + 1;
-            }else{
-                continue;
             }
-            
-
-            
-            
         }
         
 
@@ -64,6 +49,5 @@ public class MarkdownParse {
         String content = Files.readString(fileName);
         ArrayList<String> links = getLinks(content);
 	    System.out.println(links);
-        System.out.println("done");
     }
 }
