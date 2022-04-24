bb2e64aeb7b7af7276a1e75bdeb4283e23248be2
astoriama <yim016@ucsd.edu>
2022-04-24 00:57:45 -0700

    Forgot to screenshot mistake, restoring my code

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -12,6 +12,62 @@ public class MarkdownParse {
         // find the next [, then find the ], then find the (, then read link upto next )
         int currentIndex = 0;
         while(currentIndex < markdown.length()) {
+            int openBracket = markdown.indexOf("[", currentIndex);
+            int closeBracket = markdown.indexOf("]", openBracket);
+            int openParen = markdown.indexOf("(", closeBracket);
+            int closeParen = markdown.indexOf(")", openParen);
+
+            int nextOpenBracket = markdown.indexOf("[", closeParen);
+                if(nextOpenBracket!=-1){
+                    for(int i = closeParen; i<nextOpenBracket;i++){
+                    if(markdown.indexOf(")", closeParen)==-1){
+                        continue;
+                    }else{
+                        if(markdown.indexOf(")", closeParen + 1) != -1 &&markdown.indexOf(")", closeParen + 1)<nextOpenBracket){
+                            closeParen = markdown.indexOf(")", closeParen + 1);
+                        }
+                    }
+                }
+                }else{
+                    for(int i = closeParen; i<markdown.length();i++){
+                if(markdown.indexOf(")", closeParen)==-1){
+                    continue;
+                }else{
+                    if (markdown.indexOf(")", closeParen + 1) != -1){
+                        closeParen = markdown.indexOf(")", closeParen + 1);
+                    }
+                }
+            }
+        }
+
+            toReturn.add(markdown.substring(openParen + 1, closeParen));
+            currentIndex = closeParen + 1;
+
+
+
+        }
+        
+
+        return toReturn;
+    }
+
+
+    public static void main(String[] args) throws IOException {
+        Path fileName = Path.of(args[0]);
+        String content = Files.readString(fileName);
+        ArrayList<String> links = getLinks(content);
+	    System.out.println(links);
+    }
+}
+
+
+
+/** 
+    public static ArrayList<String> getLinks(String markdown) {
+        ArrayList<String> toReturn = new ArrayList<>();
+        // find the next [, then find the ], then find the (, then read link upto next )
+        int currentIndex = 0;
+        while(currentIndex < markdown.length()) {
             //need All "[", "]",and"(",")", if cannot find correct, stop.
             //1. find "[". if no, stop. Else, continue.
             if(markdown.indexOf("[", currentIndex)==-1){
@@ -39,15 +95,4 @@ public class MarkdownParse {
             }
         }
         
-
-        return toReturn;
-    }
-
-
-    public static void main(String[] args) throws IOException {
-        Path fileName = Path.of(args[0]);
-        String content = Files.readString(fileName);
-        ArrayList<String> links = getLinks(content);
-	    System.out.println(links);
-    }
-}
+*/
