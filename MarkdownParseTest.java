aace82f6017d25b4d21a6c13889ea95d602498aa
mstubbs1 <103288275+mstubbs1@users.noreply.github.com>
2022-04-29 15:16:35 -0700

    Committing fail test case

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -18,11 +18,12 @@ public class MarkdownParseTest {
     @Test
     public void testGetLinks() throws IOException {
         ArrayList<String>  l1 = new ArrayList<String>();
-        l1.add("https://something.com");
-        l1.add("some-thing.html");
+        l1.add("test.com");
+        l1.add("test(help).com");
+        l1.add("help.com")
         
         //l2 = new ArrayList<>();
-        Path fileName = Path.of("test-file.md");
+        Path fileName = Path.of("test-file9.md");
         String content = Files.readString(fileName);
         ArrayList<String> links = MarkdownParse.getLinks(content);
         
---
+++ test-file9.md
@@ -0,0 +1,6 @@
+# test 
+# test 
+[Test][test.com]
+[testing2](test(help).com)
+Ampersand([])
+[testing3](help.com)
